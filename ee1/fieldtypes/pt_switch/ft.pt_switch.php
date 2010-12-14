4f3d2e7cdc432617828b44d4c9df055c6b14e156
Brandon Kelly <me@brandon-kelly.com>
2010-12-14 12:42:02 -0800

    1.0.4

--- ee1/fieldtypes/pt_switch/ft.pt_switch.php
+++ ee1/fieldtypes/pt_switch/ft.pt_switch.php
@@ -12,7 +12,7 @@ class Pt_switch extends Fieldframe_Fieldtype {
 
 	var $info = array(
 		'name'             => 'P&T Switch',
-		'version'          => '1.0.3',
+		'version'          => '1.0.4',
 		'versions_xml_url' => 'http://pixelandtonic.com/ee/versions.xml'
 	);
 
